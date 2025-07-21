cb918449d58a8e84d654f6cbe2db72b41f4b4d55
JaviGaming8 <Javier7737302566@gmail.com>
2025-07-21 16:13:49 -0600

    Token Y Login Subir

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('✅ Conectado a MongoDB Atlas'))
+}).then(() => console.log('✅ Conectado a MongoDB Atlas con éxito'))
   .catch(err => console.error('❌ Error al conectar MongoDB:', err));
 
 // Usar rutas
