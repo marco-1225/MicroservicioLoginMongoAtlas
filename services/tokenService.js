963a9a67438f2985e9b3cd179020a4dff221cc51
JaviGaming8 <Javier7737302566@gmail.com>
2025-07-21 16:56:20 -0600

    Token

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,35 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const ACCESS_SECRET = process.env.JWT_SECRET;
-const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
-
-function generateAccessToken(usuario) {
-  return jwt.sign(
-    { id: usuario._id, nombre: usuario.Nombre },
-    ACCESS_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-  );
-}
-
-function generateRefreshToken(usuario) {
-  return jwt.sign(
-    { id: usuario._id, nombre: usuario.Nombre },
-    REFRESH_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-  );
-}
-
-function verifyAccessToken(token) {
-  return jwt.verify(token, ACCESS_SECRET);
-}
-
-function verifyRefreshToken(token) {
-  return jwt.verify(token, REFRESH_SECRET);
-}
-
-module.exports = {
-  generateAccessToken,
-  generateRefreshToken,
-  verifyAccessToken,
-  verifyRefreshToken
+const generateAccessToken = (usuario) => {
+  return jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '15m' });
 };
+
+const generateRefreshToken = (usuario) => {
+  return jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { generateAccessToken, generateRefreshToken, verifyRefreshToken };
